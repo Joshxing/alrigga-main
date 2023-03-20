2986413eefed954ccf2725fc7d0a84f37eac36ef
Josh <53200365+Joshxing@users.noreply.github.com>
2023-03-20 21:13:30 +0800

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,10 +3,10 @@ import mysql from "mysql2";
 // create the connection to database
 
 const db = mysql.createConnection({
-    host: "http://localhost:8081/",
-    user: "root",
-    password: "",
-    database: "db_restaurant"
+    host: process.env.DB_DOMAIN,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_DEFAULT_SCHEMA,
 });
 
 
