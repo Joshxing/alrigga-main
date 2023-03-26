bc9dd9f5d5d81071d2773d68c68231bf44d17ecd
Josh <53200365+Joshxing@users.noreply.github.com>
2023-03-26 13:22:42 +0800

    Update Branches.vue

--- frontend/src/pages/Branches.vue
+++ frontend/src/pages/Branches.vue
@@ -1,19 +1,30 @@
 <template>
+  <div class="select-wrapper">
     <select @change="onSelect">
       <option value="">Select Branch</option>
       <option value="branch1">Inasal</option>
       <option value="branch2">Al rigga</option>
     </select>
-  </template>
-  
+    <ul class="options">
+      <li v-if="selectedBranch === 'branch1'">Inasal</li>
+      <li v-if="selectedBranch === 'branch2'">Riqqa AI Buteen Plaza - 8 5A Street - Deira - Dubai</li>
+    </ul>
+  </div>
+</template>
+
 <script>
 export default {
+  data() {
+    return {
+      selectedBranch: "",
+    };
+  },
   methods: {
     onSelect(event) {
-      const Branches = event.target.value;
-      if (Branches === "branch1") {
+      this.selectedBranch = event.target.value;
+      if (this.selectedBranch === "branch1") {
         window.location.href = "https://phrestaurantfrontend.vercel.app";
-      } else if (Branches === "branch2") {
+      } else if (this.selectedBranch === "branch2") {
         window.location.href = "/menu";
       }
     },
@@ -22,6 +33,9 @@ export default {
 </script>
 
 <style>
+.select-wrapper {
+  position: relative;
+}
 
 select {
   background-color: #fff;
@@ -29,6 +43,30 @@ select {
   color: #333;
   font-size: 16px;
   padding: 8px;
+  width: 150px;
 }
 
+.options {
+  position: absolute;
+  top: 0;
+  left: 100%;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: none;
+}
+
+.select-wrapper:hover .options {
+  display: block;
+}
+
+.options li {
+  padding: 8px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+}
+
+.options li:hover {
+  background-color: #f5f5f5;
+}
 </style>
