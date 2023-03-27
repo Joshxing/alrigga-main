6b97b26cc340cf96b111f8382a941041516b6f1b
Josh <53200365+Joshxing@users.noreply.github.com>
2023-03-27 21:19:21 +0800

    Update Category.vue
    Commit

--- frontend/src/pages/Category.vue
+++ frontend/src/pages/Category.vue
@@ -1,78 +1,116 @@
+
 <template>
-  <div>
-    <h1>Add New Food Item</h1>
-    <form @submit.prevent="addFoodItem">
-      <label for="name">Name</label>
-      <input type="text" v-model="name" required>
 
-      <label for="description">Description</label>
-      <textarea v-model="description" required></textarea>
+<router-link @click="scrollToTop()" to="/admin" class="category">Go back</router-link>
 
-      <label for="price">Price</label>
-      <input type="number" v-model="price" required>
 
-      <label for="image">Image URL</label>
-      <input type="text" v-model="image" required>
 
-      <button type="submit">Add Food Item</button>
-    </form>
-
-    <h1>Food Items</h1>
-    <ul>
-      <li v-for="foodItem in foodItems" :key="foodItem.id">
-        <img :src="foodItem.image" alt="">
-        <h2>{{ foodItem.name }}</h2>
-        <p>{{ foodItem.description }}</p>
-        <p>{{ foodItem.price }}</p>
-      </li>
-    </ul>
-  </div>
-</template>
-
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      name: '',
-      description: '',
-      price: '',
-      image: '',
-      foodItems: []
-    }
-  },
-  mounted() {
-    this.fetchFoodItems();
-  },
-  methods: {
-    fetchFoodItems() {
-      axios.get('/api/foods')
-        .then(response => {
-          this.foodItems = response.data;
-        })
-        .catch(error => {
-          console.log(error);
-        });
+    <div class="admin-form">
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name" v-model="form.name" required>
+        </div>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <textarea class="form-control" id="description" v-model="form.description" required></textarea>
+        </div>
+        <div class="form-group">
+          <label for="price">Price</label>
+          <input type="number" class="form-control" id="price" v-model="form.price" required>
+        </div>
+        <div class="form-group">
+          <label for="number">Number</label>
+          <input type="number" class="form-control" id="number" v-model="form.number" required>
+        </div>
+        <label for="image">Choose an image:</label>
+        <input type="file" id="image" ref="image" accept="image/*">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          description: '',
+          price: null,
+          number: null
+        }
+      }
     },
-    addFoodItem() {
-      axios.post('/api/foods', {
+    methods: {
+    async submitForm() {
+      const data = {
         name: this.name,
         description: this.description,
         price: this.price,
-        image: this.image
+        number: this.number,
+        imageUrl: this.imageUrl,
+      }
+      const response = await fetch('/api/foods', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
       })
-        .then(response => {
-          this.foodItems.push(response.data);
-          this.name = '';
-          this.description = '';
-          this.price = '';
-          this.image = '';
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  }
+      if (response.ok) {
+        alert('Food item added successfully!')
+        this.name = ''
+        this.description = ''
+        this.price = ''
+        this.number = ''
+        this.imageUrl = ''
+      } else {
+        alert('Error adding food item')
+      }
+    },
+  },
 }
-</script>
+  
+  </script>
+  
+  <style>
+.category {
+    background-color: #ce4641;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 10px;
+  border-radius: 10px;
+}
+
+  .admin-form {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  
+  .form-group {
+    margin-bottom: 20px;
+  }
+  
+  label {
+    display: block;
+    font-weight: bold;
+  }
+  
+  input[type="text"],
+  textarea,
+  input[type="number"] {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+  }
+  
+  button[type="submit"] {
+    display: block;
+    margin-top: 20px;
+  }
+  </style>
